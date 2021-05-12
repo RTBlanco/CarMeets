a3f274724369457965c0f4fc9f3d2b0f63cd1f91
RTBlanco <ronnytoribio1@hotmail.com>
2021-05-12 14:04:00 -0400

    created fetch for users

--- frontend/index.js
+++ frontend/index.js
@@ -1,19 +1,52 @@
 document.addEventListener('DOMContentLoaded', ()=> {
-
+  fetchMeets();
 
 });
-
+const BASE_URL = 'http://localhost:3000'
 
 // Read 
 
 function fetchMeets(){
   // this gets the meets 
+  fetch(`${BASE_URL}/meets`)
+  .then(resp => resp.json())
+  .then(meets => {
+    for( const meet of meets){
+      console.log('rails', meet );
+      let m = new Meet(meet.id, meet.location, meet.title, meet.image, meet.owner, meet.secret_cdode, meet.date_time)
+      for(const comment of meet.comments){
+        let c = new Comment(comment.id, comment.owner, comment.content, comment.meet_id)
+        m.comments.push(c)
+        console.log(c)
+      }
+      console.log(m)
+    }
+  })
 }
 
 // create 
+function createMeet(){
 
+}
+
+function createComment(){
+
+}
 
 // update 
+function updateMeet(){
 
+}
+
+function updateComment(){
+
+}
 
 // delete
+function deleteMeet(){
+
+}
+
+function deleteeComment(){
+
+}
