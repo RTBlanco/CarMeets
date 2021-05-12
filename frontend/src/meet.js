39ad14282ceb27b05c5d68cf5830ce78b89587fb
RTBlanco <ronnytoribio1@hotmail.com>
2021-05-12 13:20:44 -0400

    created the meet class

---
+++ frontend/src/meet.js
@@ -0,0 +1,11 @@
+class Meet {
+  constructor(location, title, image, owner, secretCode, dateTime, comments){
+    this.location = location;
+    this.title = title;
+    this.image = image;
+    this.owner = owner;
+    this.secretCode = secretCode; 
+    this.dateTime = dateTime;
+    this.comments = comments;
+  }
+}
