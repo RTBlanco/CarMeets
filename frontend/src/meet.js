786b814bc81f4a343bbd47a19eb3ab1af32540cc
RTBlanco <ronnytoribio1@hotmail.com>
2021-05-12 14:17:59 -0400

    added a way to render the meet object to the dom

--- frontend/src/meet.js
+++ frontend/src/meet.js
@@ -11,8 +11,23 @@ class Meet {
   }
 
   // instance method 
-  static render(){
+  render(){
     // render the meet instance on the page 
+    const meetsDiv = document.getElementById("meets");
+    const meet = document.createElement('div');
+
+    meet.id = `meet${this.id}`;
+    meet.innerHTML = 
+    `<h1>${this.title}</h1>
+    <ul>
+      <li>location: ${this.location}</li>
+      <li>Time and date: ${this.dateTime}</li>
+      <li>image: ${this.image}</li>
+      <li>Owner: ${this.Owner}</li>
+    </ul>
+    `
+    meetsDiv.appendChild(meet)
+
   }
 
   get date(){
